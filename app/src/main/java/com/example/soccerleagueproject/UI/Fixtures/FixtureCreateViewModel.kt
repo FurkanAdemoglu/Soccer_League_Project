78920dd908012fd780b736d86b55642987e56271
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-09-11 14:05:51 +0300

    Added animations and create the Fixture Screen

---
+++ app/src/main/java/com/example/soccerleagueproject/UI/Fixtures/FixtureCreateViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.soccerleagueproject.UI.Fixtures
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.soccerleagueproject.model.Fixture
+import com.example.soccerleagueproject.model.TeamsItem
+import com.example.soccerleagueproject.util.ServiceManager
+import com.example.soccerleagueproject.util.SingleLiveEvent
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import java.lang.Exception
+
+class FixtureCreateViewModel(application: Application) : AndroidViewModel(application) {
+    private val showListMutableLiveData = MutableLiveData<List<TeamsItem>>()
+    val showListLiveData: LiveData<List<TeamsItem>> = showListMutableLiveData
+
+    val errorStateLiveData = SingleLiveEvent<String>()
+
+    fun getTeams(){
+        viewModelScope.launch {
+            try {
+                val result = ServiceManager.service.getTeamNames()
+                val showList = arrayListOf<TeamsItem>()
+                for (showResult in result) {
+                    showList.add(showResult)
+                }
+                showListMutableLiveData.postValue(showList)
+            } catch (e: Exception) {
+                errorStateLiveData.postValue("Bir hata oluştu")
+                Log.v("hata", "service call error", e)
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/soccerleagueproject/UI/List/TeamListFragment.kt
+++ app/src/main/java/com/example/soccerleagueproject/UI/List/TeamListFragment.kt
@@ -1,31 +1,67 @@
 package com.example.soccerleagueproject.UI.List
 
+import android.animation.Animator
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.soccerleagueproject.Adapters.ListViewAdapter
 import com.example.soccerleagueproject.R
+import com.example.soccerleagueproject.databinding.FragmentTeamListBinding
 
-class TeamListFragment : Fragment() {
+class TeamListFragment : Fragment(R.layout.fragment_team_list) {
 
     private val viewModel:TeamListViewModel by viewModels()
+    private lateinit var _binding:FragmentTeamListBinding
     private var adapter=ListViewAdapter()
-    private lateinit var recycler:RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTeamListBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getTeams()
-
-        recycler=view.findViewById(R.id.recycler)
-        recycler.layoutManager=LinearLayoutManager(context)
-        recycler.adapter=adapter
-
+        initView()
         observeViewModel()
+    }
+    private fun initView(){
+        _binding.teamRecycler.layoutManager=LinearLayoutManager(context)
+        _binding.teamRecycler.adapter=adapter
+        _binding.drawFixtureButton.setOnClickListener {
+            _binding.listToFixtureAnimation.visibility = View.VISIBLE
+            _binding.teamRecycler.visibility=View.GONE
+            _binding.drawFixtureButton.visibility=View.GONE
+            _binding.listToFixtureAnimation.setAnimation(R.raw.soccer)
+            _binding.listToFixtureAnimation.playAnimation()
+            _binding.listToFixtureAnimation.addAnimatorListener(object :
+                Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator?) {
+                    Log.v("AnimationStarted", "Started")
+                }
+                override fun onAnimationEnd(animation: Animator?) {
+                    val action=TeamListFragmentDirections.actionTeamListFragmentToFixturesFragment()
+                    findNavController().navigate(action)
+                }
+                override fun onAnimationCancel(animation: Animator?) {
+                    Log.v("AnimationCanceled", "Canceled")
+                }
+                override fun onAnimationRepeat(animation: Animator?) {
+                    Log.v("AnimationRepeated", "Repeated") } }
+            )
+
+        }
     }
 
     private fun observeViewModel() {
