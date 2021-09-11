8cf25fe9325baa50845546c53420d025043f33ce
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-09-11 09:01:46 +0300

    Create List Fragment And Set the layouts for fragment and item_team layout

---
+++ app/src/main/java/com/example/soccerleagueproject/UI/List/TeamListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.soccerleagueproject.UI.List
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.soccerleagueproject.R
+
+class TeamListFragment : Fragment() {
+
+}
